b67c0e83d80c80f0c69a800234055b46aefda0d3
zlaxton <zlatan.kopic171@yahoo.com>
2021-07-05 12:47:49 +0200

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,38 +1,47 @@
 let pokemonList = [
-    {
-        name: 'Bulbasaur',
-        hight: 0.7,
-        type: ['grass' , 'poison'],
+  {
+    name: 'Bulbasaur',
+    height: 0.7,
+    type: ['grass', 'poison'],
 
 
-    },
-    {
-        name: 'Snorlax',
-        height: 2.1,
-        type: ['normal'],
-          
-      },
-      {
-        name: 'Charizard',
-        height: 1.7,
-        type: ['fire','flying'],
-       
-      },
-      {
-        name: 'Mewtwo',
-        height: 2.0,
-        type: ['psychic'],
-       
-      },
-   
+  },
+  {
+    name: 'Snorlax',
+    height: 2.1,
+    type: ['normal'],
+
+  },
+  {
+    name: 'Charizard',
+    height: 1.7,
+    type: ['fire', 'flying'],
+
+  },
+  {
+    name: 'Mewtwo',
+    height: 2.0,
+    type: ['psychic'],
+
+  },
+
 
 
 
 ];
+//Unordered (buleted) list
 document.write('<ul>');
-
+//The initialization, The condition, The action
 for (let i = 0; i < pokemonList.length; i++) {
-    document.write('<li>' + pokemonList[i].name + ' ( ' + pokemonList[i].type + ')');
+  //Writes Pokemon name to the DOM, list items (Definition list)
+  document.write('<li>' + pokemonList[i].name + ' ( height :  ' + pokemonList[i].height + ') </li>');
+  //Checks if pokemon height is greater than 2 
+  if (pokemonList[i].height > 2)
+    //if pokemon height is greater than 1.6 m, writes "Wow that's big!"
+    document.write(" - Wow, that's big!");
+  //if pokemon is greater than 1 and lower than 2 writes "This is an average height"
+  else if (pokemonList[i].height > 1 && pokemonList[i].height < 2) 
+    document.write(" - This is an average height");
 
-    document.write('</li>');
-}
+
+  }
