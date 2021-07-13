1a422cb3eb9eb3a318feffa1b7136a6bf77b0d46
zlaxton <zlatan.kopic171@yahoo.com>
2021-07-13 21:05:20 +0200

    Update script.js

--- js/script.js
+++ js/script.js
@@ -29,20 +29,18 @@ let pokemonRepository = (function () {
     },
   ];
 
+  function add(pokemon) {
+    pokemonList.push(pokemon);
+  }
 
-}),
-function add(pokemon) {
-  pokemonList.push(pokemon);
-}
+  function getAll() {
+    return pokemonList;
+  }
 
-function getAll() {
-  return pokemonList;
-}
-
-return {
-  add: add,
-  getAll: getAll
-};
+  return {
+    add: add,
+    getAll: getAll
+  };
 })();
 
 console.log(pokemonRepository.getAll());
@@ -51,35 +49,5 @@ console.log(pokemonRepository.getAll());
 pokemonRepository.getAll().forEach(function (pokemon) {
   document.write(pokemon.name + " " + "height : " + pokemon.height + " " + pokemon.type + "</br>");
 });
-
-document.write('<ul>');
-
-let result
-
-//The initialization, The condition, The action
-
-for (let i = 0; i < pokemonList.length; i++) {
-
-  let pokemon = `${pokemonList[i].name} (height : ${pokemonList[i].height})`
-
-  if (pokemonList[i].height > 2)
-
-    result = `${pokemon} - Wow, that's big!)`;
-
-  else if (pokemonList[i].height > 1 && pokemonList[i].height < 2)
-
-    result = `${pokemon} - This is an avarage height)`;
-
-  else {
-    result = pokemon
-  }
-
-  document.write('<li>' + result + '</li>')
-
-
-}
-
-document.write('</ul>')
-
 
 
